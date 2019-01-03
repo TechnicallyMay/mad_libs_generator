1e81927b3bcbbf53b274f3d6e725227f6b5b8dfb
TechnicallyMay <masonwells01@gmail.com>
2019-01-02 21:27:06 -0700

    Tokenizing and POS tagging individual words.

--- main/main.py
+++ main/main.py
@@ -1,5 +1,18 @@
+import string
+import re
 import get_lyrics
+import find_words
 
 
 song = ["Fall Away", "Twenty One Pilots"]
 lyrics = get_lyrics.get_song(song[0], song[1])
+regex = re.compile('[%s]' % string.punctuation)
+no_punct = regex.sub("", lyrics)
+
+unique_words = []
+for word in no_punct.lower().split(" "):
+    if word not in unique_words:
+        unique_words.append(word)
+
+tagged = find_words.tag_pos(" ".join(unique_words))
+print(unique_words)
