988261dcbd750d38079b895a77516f2a8121089b
TechnicallyMay <masonwells01@gmail.com>
2019-01-02 23:37:46 -0700

    Can now pull apart text and find parts of speech that will be for replacing.

--- main/find_words.py
+++ main/find_words.py
@@ -1,4 +1,7 @@
 import nltk
+import re
+import string
+from collections import defaultdict
 
 
 nltk.download("punkt")
@@ -8,4 +11,24 @@ nltk.download('averaged_perceptron_tagger')
 def tag_pos(text):
     tokens = nltk.word_tokenize(text)
     tagged = nltk.pos_tag(tokens)
-    return tagged
+    clean = remove_punctuation(tagged)
+    sorted = sort_by_pos(clean)
+    return sorted
+
+
+def remove_punctuation(tagged_text):
+    no_punct = []
+    regex = re.compile('[%s]' % string.punctuation)
+    for word, pos in tagged_text:
+        clean = regex.sub("", word).lower()
+        no_punct.append((clean, pos))
+    return no_punct
+
+
+def sort_by_pos(tokens):
+    parts_of_speech = defaultdict(list)
+    punct = string.punctuation
+    for word, tag in tokens:
+        if len(word) > 2 and word not in parts_of_speech[tag]:
+            parts_of_speech[tag].append(word)
+    return parts_of_speech
