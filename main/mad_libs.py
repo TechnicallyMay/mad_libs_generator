159c709ee1436dcf9720457786d2eb9d442c8331
TechnicallyMay <masonwells01@gmail.com>
2019-01-03 00:52:46 -0700

    Made methods to randomly pick words, replace words, print.

--- main/mad_libs.py
+++ main/mad_libs.py
@@ -1,4 +1,5 @@
-import find_words
+import find_pos
+from random import choice
 from collections import defaultdict
 
 
@@ -6,7 +7,7 @@ class MadLibs():
 
     def __init__(self, text):
         self.text = text
-        tagged_text = find_words.tag_pos(text)
+        tagged_text = find_pos.tag_pos(text)
         self.pos = self.find_important_pos(tagged_text)
 
 
@@ -23,12 +24,40 @@ class MadLibs():
                 key = "Verb"
             elif tag[0] == "J":
                 key = "Adjective"
-            elif tag[0] == "P":
-                key = "Pronoun"
-            elif tag[0] == "R":
-                key = "Adverb"
             else:
-                key = "Unimportant"
+                continue
             for word in words:
                 important[key].append(word)
         return(important)
+
+
+    def pick_word(self):
+        random_key = choice(list(self.pos.keys()))
+        random_word = choice(self.pos[random_key])
+        self.pos[random_key] = [word for word in self.pos[random_key]
+                                              if word != random_word]
+        if self.pos[random_key] == []:
+            del(self.pos[random_key])
+        return (random_key, random_word)
+
+
+    def replace_word(self, to_replace, input):
+        words = self.text.split()
+        for i, word in enumerate(words):
+            new_word = input
+            if word[0].isupper():
+                new_word = new_word.capitalize()
+            if word[-1] == ',':
+                word = word.replace(",", "")
+                new_word += ","
+            if word.lower() == to_replace:
+                words[i] = new_word
+        self.text = " ".join(words)
+
+
+    def print(self):
+        for word in self.text.split():
+            if word[0].isupper():
+                print('\n' + word, end = " ")
+            else:
+                print(word, end = " ")
--- main/main.py
+++ main/main.py
@@ -1,11 +1,10 @@
 import get_lyrics
 from mad_libs import MadLibs
 
-song = ["Nico and the Niners", "Twenty One Pilots"]
-lyrics = get_lyrics.get_song(song[0], song[1])
+song = "Nico and the Niners"
+artist = "Twenty One Pilots"
+lyrics = get_lyrics.get_song(song, artist)
 
 libs = MadLibs(lyrics)
 
-for pos, words in libs.pos.items():
-    print(pos, ": ", words)
-    print("\n\n\n")
+libs.print()
